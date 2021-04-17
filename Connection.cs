789baf65bcfd3482a5d8b380df39d90c9702da82
yndvp <yunna3828@gmail.com>
2021-04-17 17:43:51 +0900

    Add project files.

---
+++ Connection.cs
@@ -0,0 +1,44 @@
+﻿/** Application Purpose: A class to define the database connection methods and variables
+* Author: Yunna Jang 200455726
+* Date: 17/4/2021
+* Time: 10:40AM
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace CollegeManagementSystem
+{
+    class Connection
+    {
+        // Set the OleDb variables related to connection
+        public OleDbConnection con;
+        public OleDbCommand cmd;
+        public OleDbDataAdapter dta;
+        public OleDbDataReader dtr;
+
+        // The connection method to be called whenever we need to make a database connection
+        public void link()
+        {
+            // Make a connection object
+            con = new OleDbConnection();
+            // Make a command object
+            cmd = new OleDbCommand();
+            // Define a connection string to the database
+            con.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source=|DataDirectory|\CollegeDB.accdb";
+            // Set the connection to the new OleDbConnection
+            cmd.Connection = con;
+
+            // Connection method that will be called when we need to open a connection
+            if(con.State != System.Data.ConnectionState.Open)
+            {
+                con.Close();
+                con.Open();
+            }
+        }
+    }
+}
---
+++ LoadingForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace CollegeManagementSystem
+{
+    public partial class LoadingForm : Form
+    {
+        public LoadingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            this.loadingpageprogressbartimer1.Start();
+        }
+
+
+        private void loadingpageprogressbartimer1_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            this.progressBar1.Maximum = 1000;
+            progressBar1.PerformStep();
+            
+            if (progressBar1.Value == 1000)
+            {
+                loadingpageprogressbartimer1.Enabled = false;   
+                LoginForm user = new LoginForm(); 
+                user.Show();
+                this.Hide();
+            }
+        }
+    }
+}
